7ff6f13a537c33b86b05c841234a53a0f6958432
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-03-24 10:38:58 -0300

    Remove tabnine and change python linter

--- lua/gus/plugins/linting.lua
+++ lua/gus/plugins/linting.lua
@@ -8,7 +8,7 @@ return {
 		local lint = require("lint")
 
 		lint.linters_by_ft = {
-			python = { "pylint" },
+			python = { "flake8" },
 			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
 			html = { "htmlhint" },
