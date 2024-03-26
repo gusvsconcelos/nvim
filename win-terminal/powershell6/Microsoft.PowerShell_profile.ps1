0663062eef7166bf5a095ceb5debdc289a7dbe82
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-03-25 21:02:50 -0300

    Set a default starter directory for powershell

--- win-terminal/powershell6/Microsoft.PowerShell_profile.ps1
+++ win-terminal/powershell6/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,3 @@
 oh-my-posh init pwsh --config "C:\Users\gluiz\AppData\Local\Programs\oh-my-posh\themes\material.omp.json" | Invoke-Expression
 New-Alias -Name vim -Value nvim
+Set-Location C:\Users\gluiz\Documentos\Repositórios
