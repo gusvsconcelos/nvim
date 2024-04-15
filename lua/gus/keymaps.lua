962e5db41eb57e9fd1d9dd56e4606ea1b3e3f1f1
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-04-15 09:14:09 -0300

    Add keymaps to toggle LSP diagnostics

--- lua/gus/keymaps.lua
+++ lua/gus/keymaps.lua
@@ -76,3 +76,22 @@ end)
 key.set("i", "<C-h>", function()
 	vim.lsp.buf.signature_help()
 end)
+
+-- Toggle LSP diagnostics
+local function hide_diagnostics()
+	vim.diagnostic.config({ -- https://neovim.io/doc/user/diagnostic.html
+		virtual_text = false,
+		signs = false,
+		underline = false,
+	})
+end
+local function show_diagnostics()
+	vim.diagnostic.config({
+		virtual_text = true,
+		signs = true,
+		underline = true,
+	})
+end
+
+vim.keymap.set("n", "<leader>dh", hide_diagnostics, { desc = "Hide LSP diagnostics" })
+vim.keymap.set("n", "<leader>ds", show_diagnostics, { desc = "Show LSP diagnostics" })
