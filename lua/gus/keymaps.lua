6c8669ee853ee44ccd14db50e3478c9c5810d4e0
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-04-24 12:56:34 -0300

    Add a function to automatically create a HTML template

--- lua/gus/keymaps.lua
+++ lua/gus/keymaps.lua
@@ -27,14 +27,6 @@ key.set(
 	{ desc = "Replace word under cursor" }
 )
 
--- Map a key to trigger the HTML template creation
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>ht",
-	"<cmd>lua Create_html_template()<CR>",
-	{ noremap = true, silent = true, desc = "Create HTML5 Template" }
-)
-
 key.set("x", "<leader>p", "'_dP", { desc = "Paste without losing the current word" })
 
 -- save file with ctrl+s
@@ -105,5 +97,56 @@ local function show_diagnostics()
 	})
 end
 
-vim.keymap.set("n", "<leader>dh", hide_diagnostics, { desc = "Hide LSP diagnostics" })
-vim.keymap.set("n", "<leader>ds", show_diagnostics, { desc = "Show LSP diagnostics" })
+key.set("n", "<leader>dh", hide_diagnostics, { desc = "Hide LSP diagnostics" })
+key.set("n", "<leader>ds", show_diagnostics, { desc = "Show LSP diagnostics" })
+
+-----------------------------------------------------
+-- Map a key to trigger the HTML template creation --
+-----------------------------------------------------
+-- FUNCTION --
+function HTML5()
+	-- Get current buffer
+	local current_buf = vim.api.nvim_get_current_buf()
+
+	-- Get the name of the current buffer
+	local buf_name = vim.api.nvim_buf_get_name(current_buf)
+
+	-- Check if the current buffer is empty
+	local is_empty = vim.api.nvim_buf_get_lines(current_buf, 0, -1, false)
+
+	-- If the buffer is not empty, ask for confirmation to overwrite
+	if #is_empty > 0 then
+		local confirm = vim.fn.confirm("Buffer is not empty. Overwrite?", "&Yes\n&No", 2)
+		if confirm ~= 1 then
+			return
+		end
+	end
+
+	-- Set the HTML template content
+	local template = [[
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Title</title>
+  </head>
+  <body></body>
+</html>
+]]
+
+	-- Replace the content of the buffer with the HTML template
+	vim.api.nvim_buf_set_lines(current_buf, 0, -1, false, vim.split(template, "\n"))
+
+	-- Set the file type as HTML
+	vim.api.nvim_buf_set_option(current_buf, "filetype", "html")
+
+	print("HTML template created successfully!")
+end
+
+-- KEYMAP --
+key.set("n", "<leader>ht", "<cmd>lua HTML5()<CR>", {
+	noremap = true,
+	silent = true,
+	desc = "Create HTML5 Template",
+})
