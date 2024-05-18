7a53077bfdfbd8d697e8f845ec621bab1fa950e7
Gus Vasconcelos <gluiz9445@gmail.com>
2024-05-18 15:18:08 -0300

    idk

--- lua/gus/plugins/colorscheme.lua
+++ lua/gus/plugins/colorscheme.lua
@@ -1,5 +1,4 @@
 return {
 	{ "folke/tokyonight.nvim" },
 	{ "akinsho/horizon.nvim" },
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
--- lua/gus/theme.lua
+++ lua/gus/theme.lua
@@ -1,4 +1,3 @@
 -- setup my editor's color scheme
 -- vim.cmd([[colorscheme tokyonight-night]])
--- vim.cmd([[colorscheme horizon]])
-vim.cmd([[colorscheme catppuccin]])
+vim.cmd([[colorscheme horizon]])
