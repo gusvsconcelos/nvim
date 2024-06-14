37af15502489aca1da61f6043c7c3f57a6048adc
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-06-14 04:25:47 -0300

    remove: windows backup file

--- sysbackup/win-terminal/powershell7/Microsoft.PowerShell_profile.ps1
+++
@@ -1,3 +0,0 @@
-oh-my-posh init pwsh --config "C:\Users\gluiz\AppData\Local\Programs\oh-my-posh\themes\material.omp.json" | Invoke-Expression
-New-Alias -Name vim -Value nvim
-New-Alias -Name py -Value python
