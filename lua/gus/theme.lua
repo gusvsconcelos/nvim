d0060f63552a1ffcf512de8c3b02ca37fe9999af
Gustavo Vasconcelos <gluiz9445@gmail.com>
2024-07-12 16:18:53 -0300

    idk

--- lua/gus/theme.lua
+++ lua/gus/theme.lua
@@ -1,3 +1,3 @@
 -- setup my editor's color scheme
--- vim.cmd([[colorscheme tokyonight-night]])
-vim.cmd([[colorscheme catppuccin-mocha]])
+vim.cmd([[colorscheme tokyonight-night]])
+-- vim.cmd([[colorscheme catppuccin-mocha]])
