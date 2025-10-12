d4a83417545236425c3da0ad7fcd0e553b9666d7
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2025-10-12 12:22:13 -0300

    remove unused colorschemes

--- lua/gustavo/plugins/colorscheme.lua
+++ lua/gustavo/plugins/colorscheme.lua
@@ -1,9 +1,3 @@
 return {
-  { 'folke/tokyonight.nvim' },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-  },
-  { 'tiagovla/tokyodark.nvim' },
+  'tiagovla/tokyodark.nvim',
 }
--- lua/gustavo/theme.lua
+++ lua/gustavo/theme.lua
@@ -1,4 +1,2 @@
 -- setup my editor's color scheme
--- vim.cmd([[colorscheme tokyonight-night]])
--- vim.cmd([[colorscheme catppuccin-mocha]])
 vim.cmd([[colorscheme tokyodark]])
