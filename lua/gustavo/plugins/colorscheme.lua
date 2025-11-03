7c7732265528bdc30eec441e9b94fbc6c53400c1
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2025-11-03 16:32:54 -0300

    add: rosé pine colorscheme

--- lua/gustavo/plugins/colorscheme.lua
+++ lua/gustavo/plugins/colorscheme.lua
@@ -1,5 +1,16 @@
 return {
-  'folke/tokyonight.nvim',
-  lazy = false,
-  priority = 1000,
+    {
+        'folke/tokyonight.nvim',
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        opts = {
+            variant = "moon",        -- or "dawn", "main"
+            disable_background = true, -- Key for transparency
+            disable_float_background = true, -- Also transparent floats
+        },
+    }
 }
