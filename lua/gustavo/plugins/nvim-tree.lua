eba3e9b9d5675fa32dc356f48dcaa47200ee5b68
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2025-11-05 00:07:58 -0300

    make lualine background transparent under tokyonight colorscheme

--- lua/gustavo/plugins/nvim-tree.lua
+++ lua/gustavo/plugins/nvim-tree.lua
@@ -24,7 +24,7 @@ return {
       view = {
         adaptive_size = false,
         side = 'left',
-        width = 30,
+        width = 26,
         preserve_window_proportions = true,
       },
       git = {
@@ -44,7 +44,7 @@ return {
         highlight_git = true,
         highlight_opened_files = 'none',
         indent_markers = {
-          enable = true,
+          enable = false,
         },
         icons = {
           show = {
