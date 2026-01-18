32514fe629e1b785605704e6228c0e9a2f96836d
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-01-18 08:00:55 -0300

    feat: add new colorscheme

--- lua/gus/plugins/colorscheme.lua
+++ lua/gus/plugins/colorscheme.lua
@@ -1,4 +1,5 @@
 return {
   { 'folke/tokyonight.nvim', lazy = false, priority = 1000 },
   { 'catppuccin/nvim', name = 'catppuccin', lazy = false, priority = 1000 },
+  { 'HoNamDuong/hybrid.nvim', lazy = false, priority = 1000 },
 }
--- lua/gus/theme.lua
+++ lua/gus/theme.lua
@@ -1,4 +1,5 @@
 local theme = vim.cmd.colorscheme
 
 -- theme('tokyonight-night')
-theme('catppuccin-macchiato')
+-- theme('catppuccin-macchiato')
+theme('hybrid')
