b64ac81e8c882cc717a12fe6976a9f249f84da6e
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-02-02 09:27:35 -0300

    simplify tree-sitter config file to a bare functional minimum

--- lua/gus/plugins/treesitter.lua
+++ lua/gus/plugins/treesitter.lua
@@ -1,26 +1,15 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
-    lazy = false,
-    build = ':TSUpdate',
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
+  lazy = false,
+  build = ':TSUpdate',
 
-    config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = {
-                'javascript',
-                'typescript',
-                'python',
-                'php',
-                'lua',
-            },
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = {},
 
-            sync_install = false,
-            auto_install = true,
-
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        })
-    end,
+      sync_install = false,
+      auto_install = true,
+    })
+  end,
 }
