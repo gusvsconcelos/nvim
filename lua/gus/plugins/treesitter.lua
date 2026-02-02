530b053663981f6de9c9d172ce02d3bcf7459c7d
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-02-02 09:10:27 -0300

    tree-sitter main -> master

--- lua/gus/plugins/treesitter.lua
+++ lua/gus/plugins/treesitter.lua
@@ -1,26 +1,26 @@
--- return {
---     'nvim-treesitter/nvim-treesitter',
---     branch = 'master',
---     lazy = false,
---     build = ':TSUpdate',
---
---     config = function()
---         require('nvim-treesitter.configs').setup({
---             ensure_installed = {
---                 'javascript',
---                 'typescript',
---                 'python',
---                 'php',
---                 'lua',
---             },
---
---             sync_install = false,
---             auto_install = true,
---
---             highlight = {
---                 enable = true,
---                 additional_vim_regex_highlighting = false,
---             },
---         })
---     end,
--- }
+return {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
+    lazy = false,
+    build = ':TSUpdate',
+
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = {
+                'javascript',
+                'typescript',
+                'python',
+                'php',
+                'lua',
+            },
+
+            sync_install = false,
+            auto_install = true,
+
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        })
+    end,
+}
