b6bfc9f8bd1da05bd6acd5f80961e4f2a2d0fba3
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-02-02 09:08:33 -0300

    try to fix tree-sitter

--- lua/gus/plugins/treesitter.lua
+++ lua/gus/plugins/treesitter.lua
@@ -1,23 +1,26 @@
-return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.config').setup({
-      ensure_installed = {
-        'javascript',
-        'typescript',
-        'c',
-        'lua',
-        'vim',
-        'vimdoc',
-        'query',
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-    })
-  end,
-}
+-- return {
+--     'nvim-treesitter/nvim-treesitter',
+--     branch = 'master',
+--     lazy = false,
+--     build = ':TSUpdate',
+--
+--     config = function()
+--         require('nvim-treesitter.configs').setup({
+--             ensure_installed = {
+--                 'javascript',
+--                 'typescript',
+--                 'python',
+--                 'php',
+--                 'lua',
+--             },
+--
+--             sync_install = false,
+--             auto_install = true,
+--
+--             highlight = {
+--                 enable = true,
+--                 additional_vim_regex_highlighting = false,
+--             },
+--         })
+--     end,
+-- }
